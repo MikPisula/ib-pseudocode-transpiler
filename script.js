23b9ade0a9410cc7d8781148730ad8e45630df07
MikPisula <MikPisula@users.noreply.github.com>
2021-10-18 18:57:40 +0200

    syntax highlighting

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ CodeMirror.defineSimpleMode("ib", {
 
       {regex: /[0-9]+/, token: "number"},
       {regex: /\/\/.*/, token: "comment"},
-
+      
       {regex: /[-+\/*=<>!]+/, token: "variable-2"},
+      {regex: /(?:mod|div)/, token: "variable-2"},
       {regex: /[A-Z_0-9]+/, token: "variable"},
     ],
 
