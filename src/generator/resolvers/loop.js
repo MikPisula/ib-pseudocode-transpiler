fb43323edf54ef8200c33b4991c92eb0774be225
MikPisula <MikPisula@users.noreply.github.com>
2021-10-21 23:22:40 +0200

    i was bored

--- src/generator/resolvers/loop.js
+++ src/generator/resolvers/loop.js
@@ -11,7 +11,15 @@ function loopfromto(resolvers, ast) {
     return `for (__vars["${ast.symbol.name}"] of __range(${resolvers[ast.to.type](resolvers, ast.to)}, ${resolvers[ast.from.type](resolvers, ast.from)})) {\n${statements}}\n`
 }
 
+function loopuntil(resolvers, ast) {
+    let condition = resolvers[ast.condition.type](resolvers, ast.condition);
+    let statements = resolvers.statements(resolvers, ast.statements);
+
+    return `while (!${condition}) {\n${statements}}\n`
+}
+
 export default {
     loopwhile,
-    loopfromto
+    loopfromto,
+    loopuntil
 }
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const read = file => fs.readFileSync(file, "utf8");
 
 const transpiler = require("../dist/cjs");
 
-describe("parser", function() {
+describe("transpiler", function() {
     const parser = peggy.generate(read("grammar.pegjs"));
 
     describe("parsing", function() {
@@ -101,42 +101,93 @@ describe("parser", function() {
 
             it(`test1`, () => assert.deepStrictEqual(lines, [24, 21, 5, 4, 1, -1]));
         })
-    })
+    
 
-    describe("io", function() {
-        let lines = [];
-        let expected = [
-            [ 21 ],
-            [ '32' ],
-            [ 'hello there' ],
-            [ 69 ],
-            [ 'hello there', 'oof' ],
-            [ [ 1, 2, 3 ] ],
-            [ 3 ],
-            [ [ 1, 2, 3, 4 ] ]
-        ];
+        describe("io", function() {
+            let lines = [];
+            let expected = [
+                [ 21 ],
+                [ '32' ],
+                [ 'hello there' ],
+                [ 69 ],
+                [ 'hello there', 'oof' ],
+                [ [ 1, 2, 3 ] ],
+                [ 3 ],
+                [ [ 1, 2, 3, 4 ] ]
+            ];
 
-        let program = transpiler.transpile(`
-            X = "oof"
+            let program = transpiler.transpile(`
+                X = "oof"
 
-            output 21
-            output "32"
-            output "hello there"
-            output 23 * 3
+                output 21
+                output "32"
+                output "hello there"
+                output 23 * 3
 
-            output "hello there", X
+                output "hello there", X
 
-            X = [1,2,3]
+                X = [1,2,3]
 
-            output X
-            output X[2]
-            output [1,2,3,4]
-        `, { debug: false, output: x => lines.push(x) });
+                output X
+                output X[2]
+                output [1,2,3,4]
+            `, { debug: false, output: x => lines.push(x) });
 
-        
-        eval(`(${program})();`);
-        console.log(lines)
+            
+            eval(`(${program})();`);
 
-        it(`test1`, () => assert.deepStrictEqual(lines, expected));
+            it(`test1`, () => assert.deepStrictEqual(lines, expected));
+        })
+
+        describe("loops", function() {
+            let lines = [];
+
+            let program = transpiler.transpile(`
+                I = 12
+                loop until I = 0
+                    output I
+                    I--
+                end loop
+
+                output "\t"
+
+                J = 2
+                loop while J < 7
+                    J = J + 1
+                    output J
+                end loop
+
+                output "\t"
+
+                loop K from 4 to 8
+                    output K
+                end loop
+            `, { debug: true, output: x => lines.push(...x) });
+
+            eval(`(${program})();`);
+            lines = lines.join(" ").split("\t").map(v => v.trim().split(" "));
+
+
+            [
+                [
+                '12', '11', '10', '9',
+                '8',  '7',  '6',  '5',
+                '4',  '3',  '2',  '1'
+                ],
+                [ '3', '4', '5', '6', '7' ],
+                [ '4', '5', '6', '7' ]
+            ]
+
+            it(`until`, () => assert.deepStrictEqual(lines[0], [
+                '12', '11', '10', '9',
+                '8',  '7',  '6',  '5',
+                '4',  '3',  '2',  '1'
+            ]))
+
+            it(`while`, () => assert.deepStrictEqual(lines[1], [ '3', '4', '5', '6', '7' ]))
+            
+            it(`from/to`, () => assert.deepStrictEqual(lines[2], [ '4', '5', '6', '7' ]))
+
+        })
     })
 })
